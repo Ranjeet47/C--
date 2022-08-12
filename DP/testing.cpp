ff4d75393e7effd3a9c1b391cd898d4ab2ea1876
Ranjeet47 <mm19b047@smail.iitm.ac.in>
2022-08-12 18:47:24 +0530

    testing DP

---
+++ DP/testing.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int max = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == max)
+        {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
